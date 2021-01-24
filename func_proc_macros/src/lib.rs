121c01ee801970439bd592abdca6821dbc5e8e6e
Damien Pontifex <damien.pontifex@gmail.com>
2021-01-24 17:20:22 +0800

    posting from actix-web

--- func_proc_macros/src/lib.rs
+++ func_proc_macros/src/lib.rs
@@ -15,7 +15,6 @@ struct TimerTriggerInputs {
 pub fn timer(args: TokenStream, item: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(item as syn::ItemFn);
     let function_ident = input.sig.ident.clone();
-    let function_name = function_ident.to_string();
     let attr_args = parse_macro_input!(args as syn::AttributeArgs);
     let TimerTriggerInputs { name, schedule } = match FromMeta::from_list(&attr_args) {
         Ok(v) => v,
@@ -23,20 +22,34 @@ pub fn timer(args: TokenStream, item: TokenStream) -> TokenStream {
             return e.write_errors().into();
         }
     };
-    
-    println!("Target {}, {}", name, schedule);
+
+    //for a in &input.sig.inputs {
+    //    println!("{:?}", a);
+    //}
+    //
+    //println!("Target {}, {}", name, schedule);
 
     let user_fn_ident = quote::format_ident!("user_{}", function_ident);
+    let service_fn = quote::format_ident!("{}_service", function_ident);
+    let service_path = format!("/{}", name);
+    //let actix_route = quote::format_ident!(#[post("/#name")]);
 
     input.sig.ident = syn::Ident::new(&user_fn_ident.to_string(), proc_macro2::Span::call_site());
     //input.block.stmts.insert(0, syn::parse_quote!(println!("Entered function {}", #function_name);));
 
     let outer_function = quote! {
-        fn #function_ident() {
+        async fn #service_fn((req, bytes): (HttpRequest, web::Bytes)) -> impl Responder {
             println!("Before user call");
+            println!("Got body {}", String::from_utf8(bytes.to_vec()).unwrap());
             let mut logger = func_types::Logger::default();
             #user_fn_ident(&mut logger);
             println!("After user call");
+            format!("Done")
+        }
+
+        fn #function_ident() -> actix_web::Resource {
+            println!("Registering {}", #service_path);
+            web::resource(#service_path).route(web::post().to(#service_fn))
         }
     };
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use func_types::Logger;
 
 #[func_proc_macros::timer(name = "MyTimer", schedule = "*/5 * * * * *")]
-fn my_timer_trigger(logger: &mut Logger) {
+fn my_timer_trigger(logger: &mut func_types::Logger) {
     logger.info("Hello, world".to_string());
 }
 
-fn main() {
-    my_timer_trigger();
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(my_timer_trigger())
+            //.route("/", web::get().to(greet))
+            //.route("/{name}", web::get().to(greet))
+    })
+    .bind((std::net::Ipv4Addr::UNSPECIFIED, 8080))?
+    .run()
+    .await
 }
+
+//fn main() {
+//    my_timer_trigger();
+//}
