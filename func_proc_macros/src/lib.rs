c5ae1f177bdf55fd9655f7a5aa91ff1e0fddb993
Damien Pontifex <damien.pontifex@gmail.com>
2021-01-25 15:02:33 +0800

    expanding arguments

--- func_proc_macros/src/lib.rs
+++ func_proc_macros/src/lib.rs
@@ -17,6 +17,14 @@ fn last_segment_in_path(path: &syn::Path) -> &syn::PathSegment {
     path.segments.last().expect("Expected at least one segment in path")
 }
 
+fn to_inputs(path_segment: &syn::PathSegment) -> (Option<proc_macro2::TokenStream>, Option<proc_macro2::TokenStream>) {
+    match path_segment.ident.to_string().as_str() {
+        "TimerInfo" => (None, Some(quote!{ body.into_inner() })),
+        "Logger" => (Some(quote! { let mut logger = func_types::Logger::default(); }), Some(quote! { &mut logger })),
+        _ => (None, None),
+    }
+}
+
 #[proc_macro_attribute]
 pub fn timer_trigger(args: TokenStream, item: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(item as syn::ItemFn);
@@ -32,19 +40,33 @@ pub fn timer_trigger(args: TokenStream, item: TokenStream) -> TokenStream {
         }
     };
 
+    let mut definitions = Vec::new();
+    let mut arguments = Vec::new();
+
     for a in &input.sig.inputs {
         match a {
             Typed(PatType { ty, .. }) => {
                 match ty.as_ref() {
                     Type::Path(syn::TypePath { path, .. }) => {
                         let arg_type_name = last_segment_in_path(&path);
-                        println!("Got pat {:#?}", arg_type_name.ident);
-                        println!("Got pat {:#?}", arg_type_name.ident == "TimerInfo");
+                        let (def, arg) = to_inputs(arg_type_name);
+                        if let Some(def) = def {
+                            definitions.push(def);
+                        }
+                        if let Some(arg) = arg {
+                            arguments.push(arg);
+                        }
                     },
                     Type::Reference(syn::TypeReference { mutability, elem, .. }) => {
                         if let Type::Path(syn::TypePath { path, .. }) = elem.as_ref() {
                             let arg_type_name = last_segment_in_path(path);
-                            println!("Reference {:#?}, {:#?}", mutability, arg_type_name.ident);
+                            let (def, arg) = to_inputs(arg_type_name);
+                            if let Some(def) = def {
+                                definitions.push(def);
+                            }
+                            if let Some(arg) = arg {
+                                arguments.push(arg);
+                            }
                         }
                     },
                     _ => {},
@@ -66,8 +88,8 @@ pub fn timer_trigger(args: TokenStream, item: TokenStream) -> TokenStream {
         #[actix_web::post(#service_path)]
         #vis async fn #function_ident((req, body): (actix_web::HttpRequest, actix_web::web::Json<func_types::TimerInfo>)) -> impl actix_web::Responder {
             println!("Before user call");
-            let mut logger = func_types::Logger::default();
-            #user_fn_ident(body.into_inner(), &mut logger);
+            #(#definitions;)*
+            #user_fn_ident(#(#arguments,)*);
             println!("After user call");
             format!("Done")
         }
